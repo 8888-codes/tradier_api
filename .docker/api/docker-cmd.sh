cf15836d35f9eb4b16ad454d6f8c3a4d2f7c0c9b
Glenn <glenn+snow_flake@mercury-wireless.com>
2018-08-19 16:44:21 -0400

    Tweaking the configs to support static files

--- .docker/api/docker-cmd.sh
+++ .docker/api/docker-cmd.sh
@@ -2,11 +2,13 @@
 
 set -ex
 
+pipenv install
+
 # Run any pending migrations
-python manage.py migrate
+pipenv run python manage.py migrate
 
 # Collect the static files we need to boot the app
-python manage.py collectstatic --clear --noinput
+pipenv run python manage.py collectstatic --clear --noinput
 
 # Boot gunicorn app with 4x workers
-gunicorn --access-logfile=- --error-logfile=- --bind=0.0.0.0:5000 --workers=4 backend.wsgi
+pipenv run gunicorn --access-logfile=- --error-logfile=- --bind=0.0.0.0:5000 --workers=4 tradier_api.wsgi
